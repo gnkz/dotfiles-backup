8a630725e498654b5b09f7d6d0d90495dc0e0533
gnkz <gnkz.dev@gmail.com>
2021-05-23 12:04:40 -0400

    update lua lsp

--- nvim/lua/lsp/lua.lua
+++ nvim/lua/lsp/lua.lua
@@ -1,6 +1,17 @@
 require"lspconfig".lua.setup {
     on_attach = function(client, bufnr)
         require"lsp".common_on_attach(client, bufnr)
-    end
+    end,
+    settings = {
+        Lua = {
+            diagnostics = {globals = {"vim"}},
+            runtime = {version = "LuaJIT", path = vim.split(package.path, ";")},
+            workspace = {
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+                }
+            }
+        }
+    }
 }
-
