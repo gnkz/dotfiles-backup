b55d8956aa7aa6a98fd292468e0ee67456edf9f4
gnkz <gnkz.dev@gmail.com>
2021-05-23 12:03:59 -0400

    install fugitive

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,11 +1,8 @@
 return require("packer").startup(function(use)
-    -- Packer can manage itself
     use "wbthomason/packer.nvim"
 
-    -- Post-install/update hook with neovim command
     use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
 
-    -- Use dependency and run lua function after load
     use {
         "lewis6991/gitsigns.nvim",
         requires = {"nvim-lua/plenary.nvim"},
@@ -18,6 +15,8 @@ return require("packer").startup(function(use)
     }
 
     use {"liuchengxu/vim-clap", run = ":Clap install-binary!"}
+
+    use "tpope/vim-fugitive"
 
     use "neovim/nvim-lspconfig"
     use "hrsh7th/nvim-compe"
