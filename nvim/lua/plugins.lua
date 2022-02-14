1fc27534f195409573c10b49c7bd98eb400351f4
gnkz <gnkz.dev@gmail.com>
2022-02-14 22:33:56 +0000

    update plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,8 +1,26 @@
 return require("packer").startup(function(use)
 	use("wbthomason/packer.nvim")
 
+	-- Tree-sitter
 	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
+	-- Statusline
+	use({
+		"hoob3rt/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
+
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+
+	-- Git
+	use("tpope/vim-fugitive")
+	use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
+	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
 	use({
 		"lewis6991/gitsigns.nvim",
 		requires = { "nvim-lua/plenary.nvim" },
@@ -11,35 +29,31 @@ return require("packer").startup(function(use)
 		end,
 	})
 
-	use({
-		"hoob3rt/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
-
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-
-	use("tpope/vim-fugitive")
-
-	use("hrsh7th/vim-vsnip")
-	use("hrsh7th/vim-vsnip-integ")
-
+	-- LSP
 	use("neovim/nvim-lspconfig")
+	use("williamboman/nvim-lsp-installer")
+	use("hrsh7th/nvim-cmp")
 	use("hrsh7th/cmp-nvim-lsp")
 	use("hrsh7th/cmp-buffer")
 	use("hrsh7th/cmp-path")
 	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-vsnip")
-	use("hrsh7th/vim-vsnip")
-	use("williamboman/nvim-lsp-installer")
 	use("nvim-lua/lsp-status.nvim")
 	use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	use({
+		"danymat/neogen",
+		config = function()
+			require("neogen").setup({
+				enabled = true,
+			})
+		end,
+	})
 
-	use({ "lukas-reineke/indent-blankline.nvim" })
+	-- Snippets
+	use("hrsh7th/cmp-vsnip")
+	use("hrsh7th/vim-vsnip")
+	use("rafamadriz/friendly-snippets")
+
+	use("lukas-reineke/indent-blankline.nvim")
 
 	use("unblevable/quick-scope")
 
@@ -48,15 +62,13 @@ return require("packer").startup(function(use)
 	use("b3nj5m1n/kommentary")
 
 	use({
-		"blackCauldron7/surround.nvim",
+		"echasnovski/mini.nvim",
+		branch = "stable",
 		config = function()
-			require("surround").setup({})
+			require("mini.surround").setup({})
 		end,
 	})
 
 	-- Theme
-	use({
-		"catppuccin/nvim",
-		as = "catppuccin",
-	})
+	use("shaunsingh/moonlight.nvim")
 end)
