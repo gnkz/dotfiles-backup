474fdeebb5e07dec3a832a85e3b4182d66158c02
gnkz <gnkz.dev@gmail.com>
2022-03-31 08:53:35 -0300

    remove fugitive.lua

--- nvim/lua/fugitive.lua
+++
@@ -1,7 +0,0 @@
-local map = require("utils").map
-
-map("n", "<leader>gs", ":G<CR>", { silent = true })
-map("n", "<leader>gd", ":Gdiff<CR>", { silent = true })
-map("n", "<leader>gb", ":Gblame<CR>", { silent = true })
-map("n", "<leader>gP", ":G push<CR>", { silent = true, nowait = true })
-map("n", "<leader>gp", ":G pull<CR>", { silent = true, nowait = true })
