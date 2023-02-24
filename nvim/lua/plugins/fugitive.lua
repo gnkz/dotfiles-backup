e35a0a5ab31e217a5195ad3c82eaadd2d8fb64c2
gnkz <gnkz.dev@gmail.com>
2023-02-23 21:33:48 -0300

    update CreateBranch function

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,6 @@
-function _create_branch()
+function CreateBranch()
 	local branch_name
-	vim.ui.input({ prompt = "Branch name:" }, function(input)
+	vim.ui.input({ prompt = "Branch name: " }, function(input)
 		branch_name = input
 	end)
 
@@ -16,13 +16,13 @@ return {
 	{
 		"tpope/vim-fugitive",
 		keys = {
-			{ "<leader>gs",  "<cmd>G<cr>",                    desc = "Git status" },
-			{ "<leader>gd",  "<cmd>Gdiff<cr>",                desc = "Git diff" },
-			{ "<leader>gb",  "<cmd>G blame<cr>",              desc = "Git blame" },
-			{ "<leader>gP",  "<cmd>G push<cr>",               desc = "Git push" },
-			{ "<leader>gp",  "<cmd>G pull<cr>",               desc = "Git pull" },
-			{ "<leader>gcb", "<cmd>lua _create_branch()<cr>", desc = "Create branch" },
-			{ "<leader>cvc", "<cmd>G commit --verbose<cr>",   desc = "Git commit" },
+			{ "<leader>gs",  "<cmd>G<cr>",                  desc = "Git status" },
+			{ "<leader>gd",  "<cmd>Gdiff<cr>",              desc = "Git diff" },
+			{ "<leader>gb",  "<cmd>G blame<cr>",            desc = "Git blame" },
+			{ "<leader>gP",  "<cmd>G push<cr>",             desc = "Git push" },
+			{ "<leader>gp",  "<cmd>G pull<cr>",             desc = "Git pull" },
+			{ "<leader>gcb", "<cmd>lua CreateBranch()<cr>", desc = "Create branch" },
+			{ "<leader>cvc", "<cmd>G commit --verbose<cr>", desc = "Git commit" },
 		}
 	}
 }
